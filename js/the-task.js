572e951be361421b19ed8f4dfad331e78019fd61
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-08-02 15:20:16 +0300

    added tasks

--- js/the-task.js
+++ js/the-task.js
@@ -19,3 +19,57 @@
 //   }
 //   console.log(messageYear);
 // }
+
+const checkYear = function (year) {
+  let condition = year % 4 === 0;
+  if (condition) return `${year} високосный`;
+  return `${year} не високосный`
+}
+console.log(checkYear(2010));
+
+
+// Задание 2 
+
+// const game = function () {
+//   let comp = Math.ceil(Math.random() * (10 - 1) + 1);
+//   let user = prompt(`Введите число от 1 до 10.`);
+//   console.log(comp, user);
+
+//   if (comp == user) return `You are win!`;
+//   return `You are lose.`;
+// }
+// console.log(game());
+
+
+// Задание 3
+
+const getMathResult = function () {
+  let num1 = prompt(`Enter the first number`);
+  let num2 = prompt(`Enter the second number`);
+  let symbol = prompt(`Enter the symbol`);
+  let total = 0;
+  console.log(num1, num2, symbol);
+
+  switch (symbol) {
+    case '+':
+      total = Number(num1) + Number(num2);
+      break;
+    
+    case '-':
+      total = num1 - num2;
+      break;
+    
+    case '*':
+      total = num1 * num2;
+      break;
+    
+    case '/':
+      total = num1 / num2;
+      break;
+    
+    default:
+      console.error(`ERROR`);
+  }
+  return total;
+}
+console.log(`result:`, getMathResult());
