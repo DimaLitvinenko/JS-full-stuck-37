ca65fc2e48bb9187674d70a1b1abaaa10d11465d
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-09-15 22:46:35 +0300

    updated

--- index.js
+++ index.js
@@ -6,28 +6,42 @@
   // 3
 // import "./js/module-3.js";                   // + Telegram Bot
   // 4
-// import "./js/module-4-callback.js"           // + Telegram Bot
-// import "./js/module-4-closures.js"
-// import "./js/module-4-arrow-&-stack.js"
-// import "./js/module-4-2-this.js"
-// import "./js/module-4-2-fn-method.js"
+// import "./js/module-4/module-4-callback.js"           // + Telegram Bot
+// import "./js/module-4/module-4-closures.js"
+// import "./js/module-4/module-4-arrow-&-stack.js"
+// import "./js/module-4/module-4-2-this.js"
+// import "./js/module-4/module-4-2-fn-method.js"
   // 5
-// import "./js/module-5-obj-prototype.js"      // + Telegram Bot
-// import "./js/module-5-constructors.js"
-// import "./js/module-5-2-classes.js"
-// import "./js/module-5-2-inheritance.js"
+// import "./js/module-5/module-5-obj-prototype.js"      // + Telegram Bot
+// import "./js/module-5/module-5-constructors.js"
+// import "./js/module-5/module-5-2-classes.js"
+// import "./js/module-5/module-5-2-inheritance.js"
   // 6
-// import "./js/module-6-array-methods.js"      
-// import "./js/module-6-telegram-bot.js"       // Telegram Bot
-// import "./js/module-6-2-lodash.js"
+// import "./js/module-6/module-6-array-methods.js"      
+// import "./js/module-6/module-6-telegram-bot.js"       // Telegram Bot
+// import "./js/module-6/module-6-2-lodash.js"
   // 7
- import "./js/module-7-DOM.js"
+//  import "./js/module-7/module-7-DOM.js"
+  // 8
+// import "./js/module-8/01-bubbling.js"
+// import "./js/module-8/02-delegation.js"
+// import "./js/module-8/03-tags.js"
+// import "./js/module-8/04-colorpicker.js"
+
+import "./js/module-8/module-8-propagation.js"
+import "./js/module-8/module-8-dubounce.js"
+import "./js/module-8/thottle.js"
+
+
 
 // PRACTICE
 
-// import "./js/practice/the-task.js";
-// import "./js/practice/practice-module-3.js"
-// import "./js/practice/counter-module-4.js"
-// import "./js/practice/counter-module-5.js"
+// import "./practice/the-task.js";
+// import "./practice/practice-module-3.js"
+// import "./practice/counter-module-4.js"
+// import "./practice/counter-module-5.js"
 
-// import "./js/practice/practice__proto__prototype.js"
+// import "./practice/practice__proto__prototype.js"
+
+// 8
+// import "./practice/intersectionObserve.js"
---
+++ references/ref.js
@@ -0,0 +1,17 @@
+export default {
+  form: document.querySelector('.form'),
+  propagation: document.querySelector('.propagation'),
+  articleProp: document.getElementById('article-prop'),
+  articleList: document.getElementById('article-list'),
+  // myBtn: document.getElementById('myBtn'),
+  // module-8 practice
+  // list
+  list: document.getElementById('list'),
+  // modal
+  modal: document.querySelector('.backDrop'),
+  // close button
+  closeBtn: document.getElementById('closeModal'),
+  modalContent: document.querySelector('.content'),
+  // form
+  form: document.getElementById('form'),
+}
